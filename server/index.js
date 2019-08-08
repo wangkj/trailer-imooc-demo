b1e0ddbfb03f591f230b8a39a960a69065f38e11
529562814@qq.com <529562814@qq.com>
2019-08-08 17:43:49 +0800

    'feat:190808'

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const Koa = require('koa')
 const app = new Koa()
+const { normal} = require('./tpl')
 // 传中间件
+// app.use(async (ctx, next) => {
+//     ctx.body = '电影首页'
+// })
+
 app.use(async (ctx, next) => {
-    ctx.body = '电影首页'
+    ctx.type = 'text/html; charset=utf-8'
+    ctx.body = normal
 })
 app.listen(4455)
---
+++ server/tpl/normal.js
@@ -0,0 +1,28 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset='utf-8'>
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <title>Koa Server HTML</title>
+            <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.1/css/bootstrap.css" rel="stylesheet">
+            <script src="https://cdn.bootcss.com/twitter-bootstrap/4.1.1/js/bootstrap.js"></script>
+            <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.js"></script>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Hi Luke</h1>
+                        <p>This is Max</p>
+                        <button class="btn">hahaha</button>
+                    </div>
+                    <div class="col-md-4">
+                        <p>测试静态html页面</p>
+                    </div>
+                </div>
+                
+            </div>
+        </body>
+    </html>
+`
