ad739e28866e6e8ade0dfd479d5dc29e2abc6d11
529562814@qq.com <529562814@qq.com>
2019-08-12 11:56:31 +0800

    'feat:新增模板'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal} = require('./tpl')
+const { normal, ejsTpl, pugTpl} = require('./tpl')
+const ejs = require('ejs')
+const pug = require('pug')
 // 传中间件
 // app.use(async (ctx, next) => {
 //     ctx.body = '电影首页'
@@ -8,6 +10,13 @@ const { normal} = require('./tpl')
 
 app.use(async (ctx, next) => {
     ctx.type = 'text/html; charset=utf-8'
-    ctx.body = normal
+    // ctx.body = ejs.render(ejsTpl, {
+    //     name1: 'Max1',
+    //     name2: 'Max2'
+    // })
+    ctx.body = pug.render(pugTpl, {
+            name1: 'Max123',
+            name2: 'Max234'
+        })
 })
 app.listen(4455)
